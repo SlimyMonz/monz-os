b613a2c8e5ac2009bd5c4cc8e9ce4d7f65553e65
Monz <code@monz.us>
2025-09-23 21:46:02 -0400

    Simplified app state because I can't figure out zIndexing.

--- src/composables/useAppState.ts
+++ src/composables/useAppState.ts
@@ -7,41 +7,30 @@ type App = {
   component: Component;
 };
 
-const openedApps = ref<App[]>([]);
 const focusedApp = ref<App | null>(null);
 
 export function useAppState() {
+
   function openApp(app: App) {
-    // If already open, just focus it
-    const alreadyOpen = openedApps.value.find(a => a.name === app.name);
-    if (!alreadyOpen) {
-      openedApps.value.push(app);
-    }
     focusedApp.value = app;
   }
 
-  function closeApp(appName: string) {
-    openedApps.value = openedApps.value.filter(app => app.name !== appName);
-    if (focusedApp.value?.name === appName) {
-      focusedApp.value = openedApps.value.at(-1) || null; // focus last opened app
-    }
-  }
-
-  function setFocus(appName: string) {
-    const app = openedApps.value.find(a => a.name === appName);
-    if (app) focusedApp.value = app;
+  function closeApp() {
+    focusedApp.value = null;
   }
 
   function isAppOpen(appName: string) {
-    return openedApps.value.some(app => app.name === appName);
+    if (focusedApp.value != null)
+    {
+      if (focusedApp.value.name == appName) return true;
+    }
+    return false;
   }
 
   return {
-    openedApps,
     focusedApp,
     openApp,
     closeApp,
-    setFocus,
     isAppOpen
   };
 }
