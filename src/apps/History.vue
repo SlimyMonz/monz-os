b35fbe2a7eb90299d808b071323c0700022c2215
Monz <code@monz.us>
2025-09-23 21:45:33 -0400

    Added fake data to test app views

--- src/apps/History.vue
+++ src/apps/History.vue
@@ -1,3 +1,70 @@
 <template>
-    <p>History</p>
+  <div class="container">
+    <p><strong>History</strong></p>
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+          <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+        <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula metus vel dolor dapibus, at facilisis justo facilisis.</p>
+      <p>Phasellus varius, odio nec fringilla aliquam, justo odio placerat urna, ut convallis risus lacus at purus.</p>
+      <p>Suspendisse potenti. Curabitur mattis risus ac purus cursus, at bibendum mauris blandit.</p>
+      <p>Donec maximus dolor et sapien scelerisque, eu pretium sapien lobortis.</p>
+      <p>Praesent vitae sapien in justo dignissim porttitor. Pellentesque habitant morbi tristique senectus et netus.</p>
+    </div>
+  </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+
+  background-color: #ccc;
+}
+
+
+</style>
