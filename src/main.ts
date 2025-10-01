6df7d538ef9adc9b7d96f96363ad2b0a8494d5c4
Monz <code@monz.us>
2025-10-01 16:30:03 -0400

    Added Tailwind CSS to app for easier component styling.

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Tailwind CSS
+import './style.css';
+
 // PrimeVue
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { ConfirmationService, DialogService, ToastService } from 'primevue';
+
+import Tooltip from 'primevue/tooltip';
 
 const app = createApp(App);
+
 app.use(PrimeVue, {
     theme: {
         preset: Aura
     }
 });
 
-createApp(App).mount('#app')
+app.directive('tooltip', Tooltip);
+
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+
+app.mount('#app');
