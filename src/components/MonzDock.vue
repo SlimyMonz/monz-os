ee26877dc1c662cb03b3bf820c5dcdf73058a699
Monz <code@monz.us>
2025-10-01 16:33:30 -0400

    Created "monzdock" and updated view to fit PrimeVue Dock type.

---
+++ src/components/MonzDock.vue
@@ -0,0 +1,23 @@
+<!-- Based off of code from: https://primevue.org/dock/ -->
+
+<template>
+    <div class="dock-window dock-advanced">
+    <Dock :model="AppList" position="left">
+        <template #item="{ item: app }">
+            <a v-tooltip.right="app.label" @click="openApp(app)" href="#" class="p-dock-item-link">
+                <img :src="app.icon" style="width: 100%" />
+            </a>
+        </template>
+    </Dock>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import Dock from 'primevue/dock';
+import { useAppState } from '@/composables/useAppState';
+import { AppList } from '@/apps/AppList';
+
+const { openApp } = useAppState();
+
+</script>
