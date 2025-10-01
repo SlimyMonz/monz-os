7e4b4c04c1e67e08f11258f26eaec2e51c1b809b
Monz <code@monz.us>
2025-10-01 16:33:13 -0400

    Created basic Desktop layout view. Added styling.

--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -1,11 +1,24 @@
 <template>
-
+    <div class="background-container">
+    <MenuBar></MenuBar>
+    <MonzDock></MonzDock>
+    </div>
 </template>
 
 <script setup lang="ts">
+import MenuBar from '@/components/MenuBar.vue';
+import MonzDock from '@/components/MonzDock.vue';
 
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+
+.background-container {
+  background-image: url('@/assets/wallpaper/monz_skin.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
 
 </style>
