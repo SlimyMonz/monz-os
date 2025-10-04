6cc13bb1f1ed74f98c85b050c94555b5933b1ee0
Monz <code@monz.us>
2025-10-04 19:29:45 -0400

    Added a few externally-opening websites. May change them to in-OS apps that allow for a clickthrough to the site. Wish I could use iFrames. :<

--- src/apps/AppList.ts
+++ src/apps/AppList.ts
@@ -5,35 +5,68 @@ import SettingsComponent from './Settings.vue';
 import SonaComponent from './Sona.vue';
 
 // Icons
-import HistoryIcon from '../assets/icons/history.svg';
-import SettingsIcon from '../assets/icons/settings.svg';
-import SonaIcon from '../assets/icons/sona.svg';
+import HistoryIcon from '@/assets/icons/history.svg';
+import SettingsIcon from '@/assets/icons/settings.svg';
+import SonaIcon from '@/assets/icons/sona.svg';
+import BlueskyIcon from '@/assets/icons/bluesky.svg';
+import FuraffinityIcon from '@/assets/icons/furaffinity.svg';
+import GithubIcon from '@/assets/icons/github.svg';
+import MastodonIcon from '@/assets/icons/mastodon.svg';
 
 // Types
 import type { MenuItem } from 'primevue/menuitem';
 import type { AppItem } from '@/types';
-import { markRaw } from 'vue';
+import { defineComponent, markRaw } from 'vue';
+
+
+const emptyComponent = markRaw(defineComponent({}));
 
 export const AppList: AppItem[] = [
-        {
-        label: "History",
-        icon: HistoryIcon,
-        component: markRaw(HistoryComponent),
-    },
     {
         label: "Settings",
         icon: SettingsIcon,
         component: markRaw(SettingsComponent),
     },
     {
+        label: "History",
+        icon: HistoryIcon,
+        component: markRaw(HistoryComponent),
+    },
+
+    {
         label: "Sona",
         icon: SonaIcon,
         component: markRaw(SonaComponent),
+    },
+    {
+        label: "Bluesky",
+        icon: BlueskyIcon,
+        component: emptyComponent,
+        command: () => window.open("https://bsky.app/profile/monz.us")
+    },
+    {
+        label: "Furaffinity",
+        icon: FuraffinityIcon,
+        component: emptyComponent,
+        command: () => window.open("https://www.furaffinity.net/user/slimymonz")
+    },
+    {
+        label: "Github",
+        icon: GithubIcon,
+        component: emptyComponent,
+        command: () => window.open("https://github.com/SlimyMonz")
+    },
+    {
+        label: "Mastodon",
+        icon: MastodonIcon,
+        component: emptyComponent,
+        command: () => window.open("https://yiff.life/@slimymonz")
     }
 ]
 
 export const DockItems: MenuItem[] = AppList.map(app => ({
     label: app.label,
     icon: app.icon,
+    command: app.command,
     app,
 }));
--- src/components/MonzDock.vue
+++ src/components/MonzDock.vue
@@ -4,13 +4,10 @@
   <div class="dock-advanced">
     <Dock :model="DockItems" position="left" style="margin: 1rem">
       <template #item="{ item }">
-        <img 
-          v-tooltip.right="item.label" 
-          :src="item.icon" 
-          :alt="(item.label as string ?? 'app icon')" 
-          style="width: 64px" 
-          class="w-16 h-16 rounded-lg object-cover cursor-pointer transition-transform hover:scale-120" 
-          @click="openApp(item.app)" />
+        <img v-tooltip.right="item.label" :src="item.icon" :alt="(item.label as string ?? 'app icon')"
+          style="width: 64px"
+          class="w-16 h-16 rounded-lg object-cover cursor-pointer transition-transform hover:scale-120"
+          @click="onDockItemClick($event, item)" />
       </template>
     </Dock>
   </div>
@@ -20,7 +17,16 @@
 import Dock from 'primevue/dock';
 import { DockItems } from '@/apps/AppList';
 import { useAppState } from '@/composables/useAppState';
+import type { MenuItem } from 'primevue/menuitem';
 
 const { openApp } = useAppState();
-</script>
 
+const onDockItemClick = (event: PointerEvent, item: MenuItem) => {
+  if (item.command) {
+    item.command({ originalEvent: event, item });
+  } else {
+    openApp(item.app);
+  }
+  event.preventDefault();
+};
+</script>
