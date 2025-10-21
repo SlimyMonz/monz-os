dc546c500e900f6472420b9acbcfd9d5ea1147b3
Monz <monz@noreply.codeberg.org>
2025-10-21 00:19:03 -0400

    Added comments and fixed styling.

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,33 +1,29 @@
 <template>
-  <nav v-if="appMenu?.length" class="flex ml-3 space-x-3">
-    <div v-for="item in appMenu" :key="item.label" class="relative group">
-      <span
-        class="text-xs font-sans text-white cursor-pointer px-3 py-1 rounded hover:bg-white/20 transition select-none"
-        @click="item.command?.()">
-        {{ item.label }}
-      </span>
-
-      <div
-        v-if="item.children?.length"
-        class="absolute left-0 mt-1 hidden group-hover:block bg-white text-gray-900 rounded shadow-lg min-w-[150px] py-1 border border-gray-300"
-      >
-        <div
-          v-for="child in item.children"
-          :key="child.label"
-          class="px-3 py-1 hover:bg-gray-300 cursor-pointer whitespace-nowrap select-none"
-          @click.stop="child.command?.()"
-        >
-          {{ child.label }}
+    <nav v-if="appMenu?.length" class="flex ml-3 space-x-3">
+        <!-- Menu Bar Parent -->
+        <div v-for="item in appMenu" :key="item.label" class="relative group">
+            <span
+                class="text-xs font-sans text-white cursor-pointer px-3 py-1 rounded-lg hover:bg-white/20 transition select-none"
+                @click="item.command?.()">
+                {{ item.label }}
+            </span>
+            <!-- Menu Bar Children -->
+            <div v-if="item.children?.length"
+                class="absolute left-0 mt-0.75 hidden group-hover:block bg-white/75 text-gray-900 rounded-xl shadow-lg min-w-[150px] border border-gray-300">
+                <div v-for="child in item.children" :key="child.label"
+                    class="px-3 py-1 m-1 hover:bg-white hover:rounded-lg cursor-pointer whitespace-nowrap select-none"
+                    @click.stop="child.command?.()">
+                    {{ child.label }}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 </template>
 
 <script setup lang="ts">
 import type { MenuItem } from '@/types';
 
 defineProps<{
-  appMenu?: MenuItem[];
+    appMenu?: MenuItem[];
 }>();
 </script>
