a765a05e51e203abc52cac5df5b14f60c5d33198
Monz <monz@noreply.codeberg.org>
2025-10-21 23:38:39 -0400

    Refactored location of menus.

---
+++ src/apps/menus/SettingsMenu.ts
@@ -0,0 +1,36 @@
+import type { MenuItem } from "@/types";
+
+const menu: MenuItem[] = [
+  {
+    label: 'Settings',
+  },
+  {
+    label: 'File',
+    children: [
+      { label: 'New File', command: () => console.log('New File clicked') },
+      { label: 'Open...', command: () => console.log('Open clicked') },
+      { label: 'Save', command: () => console.log('Save clicked') },
+      { label: 'Close', command: () => console.log('Close clicked') },
+    ],
+  },
+  {
+    label: 'Edit',
+    children: [
+      { label: 'Undo', command: () => console.log('Undo clicked') },
+      { label: 'Redo', command: () => console.log('Redo clicked') },
+      { label: 'Cut', command: () => console.log('Cut clicked') },
+      { label: 'Copy', command: () => console.log('Copy clicked') },
+      { label: 'Paste', command: () => console.log('Paste clicked') },
+    ],
+  },
+  
+  {
+    label: 'Help',
+    children: [
+      { label: 'About', command: () => console.log('About clicked') },
+      { label: 'Documentation', command: () => console.log('Documentation clicked') },
+    ],
+  },
+];
+
+export default menu;
