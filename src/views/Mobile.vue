433b0cff1afacf20f6c3a559afa692143b4929c3
Monz <monz@noreply.codeberg.org>
2025-10-22 23:53:34 -0400

    Created basic Mobile layout.

--- src/views/Mobile.vue
+++ src/views/Mobile.vue
@@ -1,11 +1,14 @@
 <template>
-
+    <MobileApp v-if="focusedApp.label !== 'Desktop'" :app="focusedApp"/>
+    <MonzLauncher v-else/>
 </template>
 
 <script setup lang="ts">
+import { useAppStore } from '@/composables/useAppState';
+import { storeToRefs } from 'pinia';
+import MobileApp from './MobileApp.vue';
+import MonzLauncher from '@/components/MonzLauncher.vue';
 
+const appStore = useAppStore();
+const { focusedApp } = storeToRefs(appStore);
 </script>
-
-<style scoped>
-
-</style>
