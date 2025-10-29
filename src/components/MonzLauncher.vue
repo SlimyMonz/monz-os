c0dd38fb50f34afcafff1dec3870e3a165ebae15
Monz <monz@noreply.codeberg.org>
2025-10-29 17:18:48 -0400

    updated mobile launcher to be bigger and better

--- src/components/MonzLauncher.vue
+++ src/components/MonzLauncher.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="flex flex-col w-full h-screen bg-[url(@/assets/wallpaper/monz_skin.jpg)] bg-center bg-cover">
-        <div class="flex flex-col p-8">
-            <!-- Date/Time Widget -->
-            <div class="flex flex-col items-center m-3 mb-10">
-                <div class="text-white bg-black/50 backdrop-blur rounded-xl p-5 text-center">
-                    <div class="text-5xl">
-                        {{ currentTime.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' }) }}
-                    </div>
-                    <div class="text-xl">
-                        {{ currentTime.toLocaleDateString(undefined, { month: 'long', year: 'numeric', day: 'numeric' })
-                        }}
-                    </div>
-                </div>
-            </div>
-            <!-- List of Apps -->
-            <div class="bg-black/10 backdrop-blur-xs rounded-2xl mx-15">
-                <div v-for="(app, index) in AppList" :key="index" class="relative m-1 p-2"
-                    @click="openAppFromItem(app)">
-                    <div class="w-18 h-18 flex items-center">
-                        <img :src="app.icon" alt="App icon" class="w-full h-full object-contain rounded-xl" />
-                        <p class="m-3 p-2 text-xl text-white bg-black/50 backdrop-blur rounded-xl">{{ app.label }}</p>
-                    </div>
-                </div>
-            </div>
+  <div class="flex flex-col w-full h-screen bg-[url(@/assets/wallpaper/monz_skin.jpg)] bg-center bg-cover">
+    <div class="flex flex-col p-5 h-full">
+      <!-- Date/Time Widget -->
+      <div class="flex flex-col items-center m-3 my-10">
+        <div class="text-white bg-black/50 backdrop-blur rounded-xl p-5 text-center">
+          <div class="text-7xl">
+            {{ currentTime.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' }) }}
+          </div>
+          <div class="text-3xl">
+            {{ currentTime.toLocaleDateString(undefined, { month: 'long', year: 'numeric', day: 'numeric' }) }}
+          </div>
         </div>
+      </div>
+
+      <!-- List of Apps -->
+      <div class="bg-black/25 backdrop-blur-xs rounded-3xl flex-1 overflow-hidden">
+        <!-- Scrollable app list -->
+        <div class="overflow-auto h-full">
+          <div
+            v-for="(app, index) in AppList"
+            :key="index"
+            class="relative p-5 m-5 bg-black/25 backdrop-blur rounded-2xl cursor-pointer"
+            @click="openAppFromItem(app)"
+          >
+            <div class="w-18 h-18 flex items-center">
+              <img :src="app.icon" alt="App icon" class="w-full h-full rounded-xl" />
+              <p class="m-5 p-2 text-3xl text-white rounded-xl">{{ app.label }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
 
 <script setup lang="ts">
 import { AppList } from '@/apps/AppList'
