7e1e66914d17644f38f3283f98230929684a40aa
Slimy Monster <code@monz.us>
2026-02-05 13:06:48 -0500

    Initalized styling to fix button location.

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,40 +1,67 @@
 <template>
-  <div class="flex items-center h-full w-full title-bar text-white cursor-move select-none" :class="maximizedStyle">
+    <div
+        class="flex items-center h-full w-full title-bar text-white cursor-move select-none"
+        :class="maximizedStyle"
+    >
+        <div class="flex-none">
+            <div v-if="maximized && menu">
+                <AppMenu :appMenu="menu"></AppMenu>
+            </div>
+            <div v-else>
+                <div class="w-[60px]"></div>
+            </div>
+        </div>
 
-    <div class="flex-1 flex-none">
-      <div v-if="maximized && menu">
-        <AppMenu :appMenu="menu"></AppMenu>
-      </div>
-      <div v-else>
-        <div class="w-[60px]"></div>
-      </div>
-    </div>
+        <div class="flex flex-1 justify-center pointer-events-none">
+            <span class="text-sm text-white font-medium truncate">{{
+                title
+            }}</span>
+        </div>
 
-    <div class="flex flex-1 justify-center pointer-events-none">
-      <span class="text-sm text-white font-medium truncate">{{ title }}</span>
+        <!-- macOS-like control buttons -->
+        <div class="flex-none mx-1 group">
+            <button
+                class="w-3 h-3 rounded-full bg-green-500 hover:bg-green-600 mx-1 cursor-pointer"
+                @click="$emit('maximize')"
+            ></button>
+            <button
+                class="w-3 h-3 rounded-full bg-yellow-400 hover:bg-yellow-500 mx-1 cursor-pointer"
+                @click="$emit('minimize')"
+            ></button>
+            <button
+                class="w-3 h-3 rounded-full bg-red-500 hover:bg-red-600 mx-1 cursor-pointer"
+                @click="$emit('close')"
+            ></button>
+        </div>
     </div>
-
-    <!-- macOS-like control buttons -->
-    <div class="flex-1 flex-none mx-1 group">
-      <button class="w-3 h-3 rounded-full bg-green-500 hover:bg-green-600 mx-1 cursor-pointer" @click="$emit('maximize')"></button>
-      <button class="w-3 h-3 rounded-full bg-yellow-400 hover:bg-yellow-500 mx-1 cursor-pointer" @click="$emit('minimize')"></button>
-      <button class="w-3 h-3 rounded-full bg-red-500 hover:bg-red-600 mx-1 cursor-pointer" @click="$emit('close')"></button>
-    </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-import type { MenuItem } from '@/types';
-import AppMenu from './AppMenu.vue';
-import { computed } from 'vue';
+import type { MenuItem } from "@/types";
+import AppMenu from "./AppMenu.vue";
+import { computed } from "vue";
 
 const maximizedStyle = computed(() => {
-  return props.maximized ? 'bg-black' : 'bg-black/70';
+    return props.maximized ? "bg-black" : "bg-black/70";
 });
 
 const props = defineProps<{
-  title: string,
-  maximized: boolean,
-  menu?: MenuItem[]
-}>()
+    title: string;
+    maximized: boolean;
+    menu?: MenuItem[];
+}>();
 </script>
+
+<style>
+.button-group {
+}
+
+.close-button {
+}
+
+.minimize-button {
+}
+
+.maximize-button {
+}
+</style>
