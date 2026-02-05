048aab395c80b9bf9d88247d392c90bdf8dc72f8
Slimy Monster <code@monz.us>
2026-02-05 13:06:32 -0500

    No longer necessary for duplicate apps for view prototyping.

--- src/apps/AppList.ts
+++ src/apps/AppList.ts
@@ -1,104 +1,50 @@
-
 // Components
-import HistoryComponent from './History.vue';
-import SettingsComponent from './Settings.vue';
-import SonaComponent from './Sona.vue';
-import SocialsComponent from './Socials.vue';
+import HistoryComponent from "./History.vue";
+import SettingsComponent from "./Settings.vue";
+import SonaComponent from "./Sona.vue";
+import SocialsComponent from "./Socials.vue";
 
 // Icons
-import HistoryIcon from '@icons/history.svg';
-import SettingsIcon from '@icons/settings.svg';
-import SonaIcon from '@icons/sona.svg';
-import SocialsIcon from '@icons/socials.svg';
+import HistoryIcon from "@icons/history.svg";
+import SettingsIcon from "@icons/settings.svg";
+import SonaIcon from "@icons/sona.svg";
+import SocialsIcon from "@icons/socials.svg";
 
 // Types
-import type { AppItem } from '@/types';
-import { markRaw } from 'vue';
+import type { AppItem } from "@/types";
+import { markRaw } from "vue";
 
 // Menus
-import SettingsMenu from './menus/SettingsMenu';
-import HistoryMenu from './menus/HistoryMenu';
-import SonaMenu from './menus/SonaMenu';
-import SocialsMenu from './menus/SocialsMenu';
-
+import SettingsMenu from "./menus/SettingsMenu";
+import HistoryMenu from "./menus/HistoryMenu";
+import SonaMenu from "./menus/SonaMenu";
+import SocialsMenu from "./menus/SocialsMenu";
 
 export const AppList: AppItem[] = [
-    {
-        label: "Settings",
-        icon: SettingsIcon,
-        component: markRaw(SettingsComponent),
-        menu: SettingsMenu
-    },
-    {
-        label: "History",
-        icon: HistoryIcon,
-        component: markRaw(HistoryComponent),
-        menu: HistoryMenu
-    },
+  {
+    label: "Settings",
+    icon: SettingsIcon,
+    component: markRaw(SettingsComponent),
+    menu: SettingsMenu,
+  },
+  {
+    label: "History",
+    icon: HistoryIcon,
+    component: markRaw(HistoryComponent),
+    menu: HistoryMenu,
+  },
 
-    {
-        label: "Sona",
-        icon: SonaIcon,
-        component: markRaw(SonaComponent),
-        menu: SonaMenu
-    },
+  {
+    label: "Sona",
+    icon: SonaIcon,
+    component: markRaw(SonaComponent),
+    menu: SonaMenu,
+  },
 
-    {
-        label: "Socials",
-        icon: SocialsIcon,
-        component: markRaw(SocialsComponent),
-        menu: SocialsMenu
-    },
-    {
-        label: "Settings",
-        icon: SettingsIcon,
-        component: markRaw(SettingsComponent),
-        menu: SettingsMenu
-    },
-    {
-        label: "History",
-        icon: HistoryIcon,
-        component: markRaw(HistoryComponent),
-        menu: HistoryMenu
-    },
-
-    {
-        label: "Sona",
-        icon: SonaIcon,
-        component: markRaw(SonaComponent),
-        menu: SonaMenu
-    },
-
-    {
-        label: "Socials",
-        icon: SocialsIcon,
-        component: markRaw(SocialsComponent),
-        menu: SocialsMenu
-    },
-        {
-        label: "Settings",
-        icon: SettingsIcon,
-        component: markRaw(SettingsComponent),
-        menu: SettingsMenu
-    },
-    {
-        label: "History",
-        icon: HistoryIcon,
-        component: markRaw(HistoryComponent),
-        menu: HistoryMenu
-    },
-
-    {
-        label: "Sona",
-        icon: SonaIcon,
-        component: markRaw(SonaComponent),
-        menu: SonaMenu
-    },
-
-    {
-        label: "Socials",
-        icon: SocialsIcon,
-        component: markRaw(SocialsComponent),
-        menu: SocialsMenu
-    },
-]
+  {
+    label: "Socials",
+    icon: SocialsIcon,
+    component: markRaw(SocialsComponent),
+    menu: SocialsMenu,
+  },
+];
